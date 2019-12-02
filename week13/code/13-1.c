8d20a74419218c17016427a658c97f8ee13cd8af
dbw <354863199@qq.com>
2019-12-01 16:52:56 -0800

    week13

---
+++ week13/code/13-1.c
@@ -0,0 +1,24 @@
+#include "my.h"
+
+void sigtoutine(int dunno)
+{
+	switch(dunno){
+		case 1:
+			printf("Get a signal SIGHUP\n");
+		case 2:
+			printf("Get a signal SIGINT\n");
+		case 3:
+			printf("Get a signal SIGQUIT\n");
+			break;
+        }
+}
+int main()
+{
+		printf("process id is %d\n",getpid());
+		signal(SIGHUP,sigtoutine);
+		signal(SIGINT,sigtoutine);
+		signal(SIGQUIT,sigtoutine);
+		while(1);
+
+
+}
