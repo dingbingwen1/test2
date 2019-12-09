296924d324edd3921e1d44cddfd8079e712718e0
dbw <354863199@qq.com>
2019-12-08 16:42:37 -0800

    week14

---
+++ week14/code/my.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <wait.h>
+#include <errno.h>
+#include <string.h>
+#include <pthread.h>
